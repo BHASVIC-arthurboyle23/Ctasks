636bfb164c43257a807c7543b0a0c3dabc187d9c
arthurboyle23 <arthur.boyle23@students.bhasvic.ac.uk>
2023-09-28 11:42:05 +0100

    randomnumbergen

---
+++ randomnumbergen/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+main() {
+    srand(time(NULL));
+    int random = ( rand() % 10);
+    printf("Please input your guess for the random number: ");
+    int guess1 = scanf("&d");
+    if
+
+}
--- untitled2/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
-main() {
-    srand(time(NULL));
-    printf("A number between 0 and 99: %d", rand() % 10);
-
-}
