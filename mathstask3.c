f9336845ba70b9d3c1adc110756250403f2aec42
arthurboyle23 <arthur.boyle23@students.bhasvic.ac.uk>
2023-10-10 12:04:41 +0100

    new

--- mathstask3.c
+++ mathstask3.c
@@ -8,22 +8,27 @@ int main() {
     char operator;
     int result;
     printf("Please enter your first number: ");
-    scanf("&d", &num1);
+    scanf("%d", &num1);
     fflush(stdin);
     printf("Please enter your second number: ");
-    scanf("&d", &num2);
+    scanf("%d", &num2);
     fflush(stdin);
     printf("Would you like to a) Add, b) Subtract, c) Mutliply,or d) Divide?: \n");
-    scanf("&c", &operator);
+    scanf("%s", &operator);
     switch(operator)
     {
-        case 'A':
+        case 'a':
             result = (num1+num2);
-            printf("..");
             break;
         case 'b':
-            result = num1+num2;
-            printf(".%d + %d = %d", num1, num2, result);
+            result = (num1-num2);
+            break;
+        case 'c':
+            result = (num1*num2);
+            break;
+        case 'd':
+            result = (num1/num2);
             break;
     }
+    printf("%d", result);
 }
