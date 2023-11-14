37820419698776cf085b1411485ab541ac858051
BHASVIC-arthurboyle23 <144803308+BHASVIC-arthurboyle23@users.noreply.github.com>
2023-11-14 00:47:35 +0000

    .

--- dinnertable/main.c
+++ dinnertable/main.c
@@ -3,61 +3,80 @@
 #include <string.h>
 
 int dining_tables[3][2];
-int bookingID;
+char bookingID[255];
 void book_a_dinner_table() {
+    printf("Please enter your Booking ID: ");
     fflush(stdin);
-    bookingID = scanf("Please enter your bookingID: ");
+    scanf("%s", bookingID);
     int i, x, time;
     char meal_sitting[10];
     char board_type[3];
     printf("Are you staying in an FB, HB, or BB arrangement?: ");
     fflush(stdin);
-    scanf("%s", &board_type);
+    scanf("%s", board_type);
+    for (i = 0; i < 3; i++) {
+        for (x = 0; x < 2; x++) {
+            dining_tables[i][x] = 0;
+        }
+    }
     if (strcmp(board_type, "FB") == 0 || strcmp(board_type, "HB") == 0) {
         printf("Available Tables:\n");
         for (i = 0; i < 3; i++) {
             for (x = 0; x < 2; x++) {
                 if (i == 0) {
                     printf("Table Endor at ");
-                } else if (i == 1) {
+                }
+                else if (i == 1) {
                     printf("Table Naboo at ");
-                } else {
+                }
+                else {
                     printf("Table Tatooine at ");
                 }
 
                 if (x == 0) {
-                    printf("7 pm\n");
-                } else {
-                    printf("9 pm\n");
+                    printf("7pm: ");
+                }
+                else {
+                    printf("9pm: ");
+                }
+                if (dining_tables[i][x] == 0) {
+                    printf("Available\n");
+                }
+                else {
+                    printf("Taken\n");
                 }
             }
         }
-
         printf("Please choose a table (Endor, Naboo, or Tatooine) and a time (7 or 9) for reservation: ");
         fflush(stdin);
         scanf("%s %d", meal_sitting, &time);
         int table;
         if (strcmp(meal_sitting, "Endor") == 0) {
             table = 0;
-        } else if (strcmp(meal_sitting, "Naboo") == 0) {
+        }
+        else if (strcmp(meal_sitting, "Naboo") == 0) {
             table = 1;
-        } else if (strcmp(meal_sitting, "Tatooine") == 0) {
+        }
+        else if (strcmp(meal_sitting, "Tatooine") == 0) {
             table = 2;
-        } else {
+        }
+        else {
             table = -1;
         }
-
         if (table >= 0 && table < 3 && (time == 7 || time == 9)) {
             if (dining_tables[table][time / 7 - 1] == 0) {
                 printf("Reservation confirmed for Table %s at %d pm.\n", meal_sitting, time);
                 dining_tables[table][time / 7 - 1] = 1;
-            } else {
+            }
+            else {
                 printf("The selected table and time are not available.\n");
             }
-        } else {
+        }
+        else {
             printf("Invalid choice of table or time.\n");
         }
-    } else {
+    }
+    else {
         printf("Sorry, only FB and HB can book a reservation.\n");
     }
 }
